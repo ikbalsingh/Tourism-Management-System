cb2c762b760687e2349524a2acf65146a6a22cad
AnandGH5 <anandganeshgx97@gmail.com>
2017-08-21 01:37:59 +0530

    -a

--- Main website/js/index.js
+++ Main website/js/index.js
@@ -1,6 +1,38 @@
+var imgSrcs =[
+
+    "images/mainpage1.jpg",
+    "images/mainpage2.jpg",
+    "images/mainpage3.jpg",
+    "images/mainpage4.jpg",
+    "images/mainpage.jpg"
+];
+
+
+
+
 $(document).ready(function(){
     $(".button-collapse").sideNav();
-     $('.maincontent').animateCss('fadeIn');
+    
+
+
+
+$('#main').delay(10).fadeIn(1000, animateBackground());
+
+function animateBackground() {
+
+    window.setTimeout(function(){
+
+        var url = imgSrcs[imgSrcs.push(imgSrcs.shift()) - 1];
+
+        $('#main').delay(3000).fadeOut(600, function(){
+
+            $(this).css("background-image", "url(" + url + ")")
+
+        }).fadeIn(2000, animateBackground())
+
+    });
+}
+      
 });
 
 
